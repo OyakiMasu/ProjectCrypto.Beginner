df1d3493e27cc0df118034401130cbe735b998f7
OyakMasu21 <akzaki17@gmail.com>
2022-12-19 12:52:13 +0300

    Added event click to the home and crypto lidt

--- js/index.js
+++ js/index.js
@@ -7,9 +7,51 @@ const MARKET = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&o
 const EXCHANGES = "https://api.coingecko.com/api/v3/exchanges"
 
 document.addEventListener('DOMContentLoaded', () => {
+//MY DATA THAT I AM CONSTANTLY CALLING
+const mainPage = document.getElementById('Crypto')
+const cryptoCoins = document.getElementById('coin-list')
+const cryptoExchanges = document.getElementById('market-value')
+const searchResults = document.getElementById('search-result')
 
-//Create entry of cryptos
+//LINKS THAT I WILL USE FOR THE EVENTS
+const homeLink = document.getElementById('home-link')
+const cryptolistLink = document.getElementById('crypto-link')
+const exchangeLink = document.getElementById('exchange-link')
+const homecryptoIntro = document.getElementById('Crypto')
 
+//Search Engine
+const searchForm = document.getElementById('search-form')
+const searchBox = document.getElementById('search')
+
+
+//Click event for the crypto list
+cryptolistLink.addEventListener('click', () => {
+  //hide main page
+mainPage.style.display = "none"
+  //hide exchanges
+cryptoExchanges.style.display="none"
+  //hide search
+ searchBox.style.display="none"
+  //show Crypto List
+cryptoCoins.removeAttribute('hidden')  
+cryptoCoins.style.display= "flex"  
+
+})
+
+//Click event for the home page
+homeLink.addEventListener('click', () => {
+//hide crypto page
+cryptoCoins.style.display = "none"
+//hide exchanges
+cryptoExchanges.style.display = "none"
+//hide search 
+ searchBox.style.display="none"
+//show home
+homecryptoIntro.removeAttribute('hidden')
+homecryptoIntro.style.display = "flex"
+
+
+})
 
 //Load crypto
 const loadCrypto = () => {
@@ -27,11 +69,12 @@ const loadCrypto = () => {
         for(i=0 ;i<cryptoData.length; i++){
             let cryptoName = document.createElement('ul')
             cryptoName.innerHTML=
-             `                     <li>Symbol : ${cryptoData[i].symbol}</li> 
-                                   <li>Name : ${cryptoData[i].name}</li>
-                                   <li>Market value : ${cryptoData[i].priceUsd}$
-                                                              `
-            document.getElementById('coin-list').append(cryptoName)
+             `      
+                   <li>Symbol : ${cryptoData[i].symbol}</li> 
+                    <li>Name : ${cryptoData[i].name}</li>
+                    <li>Market value : ${cryptoData[i].priceUsd}$
+            `
+            cryptoCoins.append(cryptoName)
             // let cryptoSymbol = document.createElement('ul')
             // cryptoSymbol.innerHTML = `<li>${cryptoData[i].symbol}</li>`
             // document.getElementById('coin-list').append(cryptoSymbol)
@@ -40,6 +83,14 @@ const loadCrypto = () => {
 
         })
 }    
+
+
+
+
+
+
+
+//Load Crypto list
  loadCrypto();
 
 })
